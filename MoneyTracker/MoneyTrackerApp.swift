edc6bafc92f4a3d0656724fd13a7eea998803cea
mrdanwa <wdanrui@gmail.com>
2025-02-06 22:30:22 +0100

    Initial Commit

---
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Danrui Wang on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
